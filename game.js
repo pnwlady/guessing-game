b3ddbd75ec1e9d2a6101d3e2688c0ac2829f345c
Heidi Laursen <heidilaursen@yahoo.com>
2015-09-30 14:15:29 -0700

    Guessing game with added error correction .toLowerCase()

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 
 var question1 = prompt('Did Heidi grow up in Alaska?');
+var question1 = question1.toLowerCase();
+console.log('Question 1 response: ' + question1);
 if (question1 == 'no') {
   alert('You have chosen wisely.');
 } else {
@@ -7,6 +9,8 @@ if (question1 == 'no') {
 }
 
 var question2 = prompt('Did Heidi vote for Bush?');
+var question2 = question2.toLowerCase();
+console.log('Question 2 response: ' + question2);
 if (question2 == 'no') {
   alert('You\'re smarter than the average Bush.');
 } else {
@@ -14,17 +18,15 @@ if (question2 == 'no') {
 }
 
 var question3 = prompt('Does Heidi have a rabbit?');
-if (question3 == 'no') {
+var question3 = question3.toLowerCase();
+console.log('Question 3 response: ' + question3);
+if (question3.toLowerCase() == 'no') {
   alert('You\'re right, she had one named Randall.');
 } else {
   alert('Not right now... but it\'s only a matter of time.');
 }
 
 /*
-console.log('Question 1 response: ' + question1);
-console.log('Question 2 response: ' + question2);
-console.log('Question 3 response: ' + question3);
-
 alert('Your answer to Question 1 was ' + question1 + '.');
 alert('Your answer to Question 2 was ' + question2 + '.');
 alert('Your answer to Question 3 was ' + question3 + '.');
