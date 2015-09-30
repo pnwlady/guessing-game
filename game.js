c317246bac652bfb2449612e44b28eb774de4bed
Heidi Laursen <heidilaursen@yahoo.com>
2015-09-30 13:57:50 -0700

    Guessing game with added if..else statements

--- game.js
+++ game.js
@@ -1,7 +1,26 @@
-var question1 = prompt('Did Heidi grow up in Alaska?');
-var question2 = prompt('Did Heidi vote for Bush?');
-var question3 = prompt('Does Heidi have a rabbit?');
 
+var question1 = prompt('Did Heidi grow up in Alaska?');
+if (question1 == 'no') {
+  alert('You have chosen wisely.');
+} else {
+  alert('She wishes.');
+}
+
+var question2 = prompt('Did Heidi vote for Bush?');
+if (question2 == 'no') {
+  alert('You\'re smarter than the average Bush.');
+} else {
+  alert('Ouch!');
+}
+
+var question3 = prompt('Does Heidi have a rabbit?');
+if (question3 == 'no') {
+  alert('You\'re right, she had one named Randall.');
+} else {
+  alert('Not right now... but it\'s only a matter of time.');
+}
+
+/*
 console.log('Question 1 response: ' + question1);
 console.log('Question 2 response: ' + question2);
 console.log('Question 3 response: ' + question3);
@@ -9,3 +28,4 @@ console.log('Question 3 response: ' + question3);
 alert('Your answer to Question 1 was ' + question1 + '.');
 alert('Your answer to Question 2 was ' + question2 + '.');
 alert('Your answer to Question 3 was ' + question3 + '.');
+*/
