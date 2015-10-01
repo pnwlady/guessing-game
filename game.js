5e6d985ca57a2e242ab74d729d22f419bd68176c
John Cody Nemmer <jonemmer@hotmail.com>
2015-10-01 13:48:55 -0700

    Move Alert updates. Yet again, like a boss!

--- game.js
+++ game.js
@@ -1,5 +1,9 @@
 var answersCorrect = 0;
 
+var one = document.getElementById("one");
+var two = document.getElementById("two");
+var three = document.getElementById("three");
+
 
 
 function ques1() {
@@ -7,11 +11,11 @@ function ques1() {
   var question1 = question1.toLowerCase();
   console.log('Question 1 response: ' + question1);
   if (question1 === 'no' || question1 === 'n') {
-    alert('You have chosen wisely.');
+    one.innerHTML = 'You have chosen wisely.';
     answersCorrect++;
     console.log(answersCorrect);
   } else {
-    alert('She wishes.');
+    one.innerHTML = 'She wishes.';
   }  
 }
  ques1();
@@ -21,11 +25,11 @@ function ques2() {
   var question2 = question2.toLowerCase();
   console.log('Question 2 response: ' + question2);
   if (question2 === 'no' || question2 === 'n') {
-    alert('You\'re smarter than the average Bush.');
+    two.innerHTML = 'You\'re smarter than the average Bush.';
     answersCorrect++;
     console.log(answersCorrect);
   } else {
-    alert('Ouch!');
+    two.innerHTML = 'Ouch!';
   }  
 }
 
@@ -36,12 +40,14 @@ function ques3() {
   var question3 = question3.toLowerCase();
   console.log('Question 3 response: ' + question3);
   if (question3 === 'no' || question3 === 'no') {
-    alert('You\'re right, she had one named Randall.');
+    three.innerHTML = 'You\'re right, she had one named Randall.';
     answersCorrect++;
     console.log(answersCorrect);
   } else {
-    alert('Not right now... but it\'s only a matter of time.');
+    three.innerHTML = 'Not right now... but it\'s only a matter of time.';
     }  
   }
+
 ques3();
+
 alert('You got ' + answersCorrect + ' out of 3 correct. Thanks for playing!')
