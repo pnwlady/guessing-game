074d039358de5e665bd7434b22af9ce009b51a75
Jared Stratton <Strattonshrugged@gmail.com>
2015-10-02 14:06:24 -0700

    Add images to display upon correct responses

--- game.js
+++ game.js
@@ -10,6 +10,9 @@ var one = document.getElementById("one");
 var two = document.getElementById("two");
 var three = document.getElementById("three");
 
+var imageOne = document.getElementById("moose");
+var imageTwo = document.getElementById("bush");
+var imageThree = document.getElementById("rabbit");
 
 
 function ques1() {
@@ -17,6 +20,7 @@ function ques1() {
   var question1 = question1.toLowerCase();
   console.log('Question 1 response: ' + question1);
   if (question1 === answer[2] || question1 === answer[3]) {
+    imageOne.classList.remove('incorrect');
     one.innerHTML = 'You have chosen wisely.';
     answersCorrect++;
     console.log(answersCorrect);
@@ -31,6 +35,7 @@ function ques2() {
   var question2 = question2.toLowerCase();
   console.log('Question 2 response: ' + question2);
   if (question2 === answer[2] || question2 === answer[3]) {
+    imageTwo.classList.remove('incorrect');
     two.innerHTML = 'You\'re smarter than the average Bush.';
     answersCorrect++;
     console.log(answersCorrect);
@@ -39,13 +44,14 @@ function ques2() {
   }  
 }
 
-ques2();
+window.setTimeout(ques2, 1000);
 
 function ques3() {
   var question3 = prompt(question[2]);
   var question3 = question3.toLowerCase();
   console.log('Question 3 response: ' + question3);
   if (question3 === answer[2] || question3 === answer[3]) {
+    imageThree.classList.remove('incorrect');
     three.innerHTML = 'You\'re right, she had one named Randall.';
     answersCorrect++;
     console.log(answersCorrect);
@@ -54,6 +60,10 @@ function ques3() {
     }  
   }
 
-ques3();
+window.setTimeout(ques3, 1000);
 
-alert('You got ' + answersCorrect + ' out of 3 correct. Thanks for playing!')
+function summary()  {
+  alert('You got ' + answersCorrect + ' out of 3 correct. Thanks for playing!');
+}
+
+window.setTimeout(summary, 2000);
