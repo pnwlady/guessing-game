a3fb02a75a90bae389bdc7bcaf4f8d5af093131c
Jared Stratton <Strattonshrugged@gmail.com>
2015-10-02 14:17:02 -0700

    Added color with css styling

--- game.js
+++ game.js
@@ -20,11 +20,13 @@ function ques1() {
   var question1 = question1.toLowerCase();
   console.log('Question 1 response: ' + question1);
   if (question1 === answer[2] || question1 === answer[3]) {
-    imageOne.classList.remove('incorrect');
+    imageOne.classList.remove('hidden');
+    one.className = 'correct';
     one.innerHTML = 'You have chosen wisely.';
     answersCorrect++;
     console.log(answersCorrect);
   } else {
+    one.className = 'incorrect';
     one.innerHTML = 'She wishes.';
   }  
 }
@@ -35,11 +37,13 @@ function ques2() {
   var question2 = question2.toLowerCase();
   console.log('Question 2 response: ' + question2);
   if (question2 === answer[2] || question2 === answer[3]) {
-    imageTwo.classList.remove('incorrect');
+    imageTwo.classList.remove('hidden');
+    two.className = 'correct';
     two.innerHTML = 'You\'re smarter than the average Bush.';
     answersCorrect++;
     console.log(answersCorrect);
   } else {
+    two.className = 'incorrect';
     two.innerHTML = 'Ouch!';
   }  
 }
@@ -51,11 +55,13 @@ function ques3() {
   var question3 = question3.toLowerCase();
   console.log('Question 3 response: ' + question3);
   if (question3 === answer[2] || question3 === answer[3]) {
-    imageThree.classList.remove('incorrect');
+    imageThree.classList.remove('hidden');
+    three.className = 'correct';
     three.innerHTML = 'You\'re right, she had one named Randall.';
     answersCorrect++;
     console.log(answersCorrect);
   } else {
+    three.className = 'incorrect';
     three.innerHTML = 'Not right now... but it\'s only a matter of time.';
     }  
   }
